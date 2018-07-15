3908c1b849b49d48f7d3c151fb3fecf96807f323
SandeepkumarB-hexaware <31004610+sandeephexa@users.noreply.github.com>
2018-07-15 13:07:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'techguyweb';
+const defaultChannel = 'GoogleDevelopers';
 
 // load Auth2 library
 function handleClientLoad(){
