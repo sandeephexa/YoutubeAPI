b72668ca901c4f1bc9030ae130f757dda9e4a577
SandeepkumarB-hexaware <31004610+sandeephexa@users.noreply.github.com>
2018-07-15 13:01:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -72,20 +72,34 @@ function handleSignoutClick() {
         gapi.auth2.getAuthInstance().signOut();
 }
 
-
+ function showChannelData(data){
+   const channelData = document.getElementById('#channel-data');
+   channelData.innerHTML = data;
+ }
       /**
        * Print files. new line
        */
       function getChannel() {
         gapi.client.youtube.channels.list({
           'part': 'snippet,contentDetails,statistics',
-          'forUsername': 'GoogleDevelopers'
+          'forUsername': channel
         }).then(function(response) {
           console.log(response);
-          // var channel = response.result.items[0];
-          // appendPre('This channel\'s ID is ' + channel.id + '. ' +
-          //           'Its title is \'' + channel.snippet.title + ', ' +
-          //           'and it has ' + channel.statistics.viewCount + ' views.');
+          const channel = response.result.items[0];
+          const output = `<ul class="collection">
+          <li class="collection-item">Title : ${channel.snippet.title}</li>
+          <li class="collection-item">ID : ${channel.id}</li>
+          <li class="collection-item">Subscribers : ${channel.statistics.subscriberCount}</li>
+          <li class="collection-item">Views : ${channel.statistics.viewCount}</li>
+          <li class="collection-item">Videos :  ${channel.statistics.videoCount}</li>
+          </ul>
+          <p>${channel.snippet.description}</p>
+          <hr>
+          <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
+            channel.snippet.customUrl
+          }">Visit Channel</a>`;
+          showChannelData(output);
+         
         })
         .catch(function(err){
           alert('no channels by that name');
